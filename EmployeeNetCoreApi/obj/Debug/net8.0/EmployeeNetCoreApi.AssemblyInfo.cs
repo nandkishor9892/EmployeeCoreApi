d31992cfed2b4b5ac296347821edfc541e319557
Nandkishor <nandkishor175@gmail.com>
2026-08-20 15:50:39 +0530

    chages space in employee controller

--- EmployeeNetCoreApi/obj/Debug/net8.0/EmployeeNetCoreApi.AssemblyInfo.cs
+++ EmployeeNetCoreApi/obj/Debug/net8.0/EmployeeNetCoreApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeNetCoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaea7c39502f7867d823342e055992dbeab14336")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeNetCoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeNetCoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
