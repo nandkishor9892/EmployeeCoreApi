c9a63c9c1d14f7ec76ffd90c5519247627e4e9e1
Nandkishor <nandkishor175@gmail.com>
2026-08-22 10:54:38 +0530

    stagw vommit

--- EmployeeNetCoreApi/obj/Debug/net8.0/EmployeeNetCoreApi.AssemblyInfo.cs
+++ EmployeeNetCoreApi/obj/Debug/net8.0/EmployeeNetCoreApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeNetCoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8d703dee584a78e621c14cd429299a5a875e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af2279d518e1fb94cc5201d99473fa6adf288f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeNetCoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeNetCoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
